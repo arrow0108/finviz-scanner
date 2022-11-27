a53f98362cb60cc2b2c8105ed41412c930a19bc7
L.A <2095309+arrow0108@users.noreply.github.com>
2022-11-27 06:59:22 +0200

    initial code
    copy local file

---
+++ finviz-scanner.py
@@ -0,0 +1,25 @@
+import requests
+import csv
+import time
+
+# Globals
+NEW_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=cap_smallover,ind_stocksonly,ipodate_01-01-2007x,sh_price_o30,ta_highlow52w_nh,ta_perf_4w20o,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+TEN_PERCENT_TO_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=cap_smallover,ind_stocksonly,ipodate_01-01-2007x,sh_price_o30,ta_highlow52w_b0to10h,ta_perf_4w20o,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+
+
+def scan(URL):
+    url = URL + os.environ.get(USERMAIL, '')
+    response = requests.get(URL)
+    open("export.csv", "wb").write(response.content)
+    with open("export.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        first_line_read = False
+        for row in csv_reader:
+            if not first_line_read:
+                first_line_read = True
+                continue
+            print(row[1])
+
+scan(NEW_52_HIGH_SCAN)
+time.sleep(3)
+scan(TEN_PERCENT_TO_52_HIGH_SCAN)
