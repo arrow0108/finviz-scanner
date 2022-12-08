831ac5e989686fe105c8f7b09391d9816d748900
admin <admin@h-MacBook-Pro-sl-admin.local>
2022-12-08 07:57:23 +0200

    customize filters

--- finviz-scanner.py
+++ finviz-scanner.py
@@ -4,10 +4,18 @@ import click
 import time
 
 # Globals
-NEW_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=sh_avgvol_o500,ta_beta_o0.5,cap_smallover,ind_stocksonly,ipodate_01-01-2002,sh_price_o30,ta_highlow52w_nh,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
-TEN_PERCENT_TO_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=sh_avgvol_o500,ta_beta_o0.5,cap_smallover,ind_stocksonly,ipodate_01-01-2002,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
-PRCT70_ABOVE_52W_LOW_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=sh_avgvol_o500,ta_beta_o0.5,cap_smallover,ind_stocksonly,ipodate_01-01-2002,sh_price_o30,ta_highlow52w_a70h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+NEW_52_HIGH_SCAN_EPS30 = 'https://elite.finviz.com/export.ashx?f=fa_epsqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+NEW_52_HIGH_SCAN_SALES30 = 'https://elite.finviz.com/export.ashx?f=fa_salesqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
 
+TEN_PERCENT_TO_52_HIGH_SCAN_EPS30 = 'https://elite.finviz.com/export.ashx?v=111&f=fa_epsqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+TEN_PERCENT_TO_52_HIGH_SCAN_SALES30 = 'https://elite.finviz.com/export.ashx?v=111&f=fa_salesqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+
+PRCT70_ABOVE_52W_LOW_SCAN_EPS30 = 'https://elite.finviz.com/export.ashx?f=fa_epsqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+PRCT70_ABOVE_52W_LOW_SCAN_SALES30 = 'https://elite.finviz.com/export.ashx?f=fa_salesqoq_o30,sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_beta_o0.5,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+
+PERF_MONTH_ABOVE_20 = 'https://elite.finviz.com/export.ashx?f=cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_avgvol_o500,sh_price_o30,ta_beta_o0.5,ta_perf_4w20o,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+
+FILTERS = [PERF_MONTH_ABOVE_20, NEW_52_HIGH_SCAN_EPS30, NEW_52_HIGH_SCAN_SALES30, TEN_PERCENT_TO_52_HIGH_SCAN_EPS30, TEN_PERCENT_TO_52_HIGH_SCAN_SALES30, PRCT70_ABOVE_52W_LOW_SCAN_EPS30, PRCT70_ABOVE_52W_LOW_SCAN_SALES30]
 def scan(URL, username):
     url = f"{URL}{username}"
     response = requests.get(url)
@@ -24,11 +32,9 @@ def scan(URL, username):
 @click.command()
 @click.option('--username', default="user@email", help='FinViz User Email')
 def print_scan_results(username):
-    scan(NEW_52_HIGH_SCAN, username)
-    time.sleep(3)
-    scan(TEN_PERCENT_TO_52_HIGH_SCAN, username)
-    time.sleep(3)
-    scan(PRCT70_ABOVE_52W_LOW_SCAN, username)
+    for filter in FILTERS:
+        scan(filter, username)
+        time.sleep(3)
 
 if __name__ == '__main__':
     print_scan_results()
