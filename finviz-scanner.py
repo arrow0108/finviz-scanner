8711c1b49b2cc38e588810f2b6fcab8f49846b04
admin <admin@h-MacBook-Pro-sl-admin.local>
2022-12-10 19:24:22 +0200

    EPS over 25%

--- finviz-scanner.py
+++ finviz-scanner.py
@@ -4,13 +4,13 @@ import click
 import time
 
 # Globals
-NEW_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?f=sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+NEW_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=fa_epsqoq_high,cap_midover,ind_stocksonly,sh_avgvol_o300,sh_price_o30,ta_highlow52w_nh,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
 
-TEN_PERCENT_TO_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+TEN_PERCENT_TO_52_HIGH_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=fa_epsqoq_high,sh_avgvol_o300,cap_midover,ind_stocksonly,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
 
-PRCT70_ABOVE_52W_LOW_SCAN = 'https://elite.finviz.com/export.ashx?f=sh_avgvol_o500,cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+PRCT70_ABOVE_52W_LOW_SCAN = 'https://elite.finviz.com/export.ashx?v=111&f=fa_epsqoq_high,sh_avgvol_o300,cap_midover,ind_stocksonly,sh_price_o30,ta_highlow52w_b0to10h,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
 
-PERF_MONTH_ABOVE_20 = 'https://elite.finviz.com/export.ashx?f=cap_smallover,ind_stocksonly,ipodate_01-01-2002x,sh_avgvol_o500,sh_price_o30,ta_perf_4w20o,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
+PERF_MONTH_ABOVE_20 = 'https://elite.finviz.com/export.ashx?v=111&f=cap_midover,ind_stocksonly,sh_avgvol_o300,sh_price_o30,ta_perf_4w20o,ta_sma200_sb50,ta_sma50_pa&ft=4&auth='
 
 FILTERS = [PERF_MONTH_ABOVE_20, NEW_52_HIGH_SCAN, TEN_PERCENT_TO_52_HIGH_SCAN, PRCT70_ABOVE_52W_LOW_SCAN]
 def scan(URL, username):
