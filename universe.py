9033faab3f5407e4d918f88d5ad54ca53d8597b7
admin <admin@h-MacBook-Pro-sl-admin.local>
2022-12-28 23:29:44 +0200

    forte finviz params

--- universe.py
+++ universe.py
@@ -4,7 +4,7 @@ import click
 import time
 
 # Globals
-UNIVERSE = 'https://elite.finviz.com/export.ashx?v=151&f=ind_stocksonly,sh_avgvol_o500,sh_short_o5,ta_beta_o1,ta_sma200_sb50,ta_sma50_pa&auth='
+UNIVERSE = 'https://elite.finviz.com/export.ashx?v=151&f=ind_stocksonly,sh_avgvol_o500,sh_short_o5,ta_beta_o1,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&auth='
 
 FILTERS = [UNIVERSE]
 def scan(URL, username):
